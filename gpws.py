2f11222650b29ee942370418f3b6c39076fc362f
Simon <simon.aldouri.ravoux@gmail.com>
2016-12-13 15:00:03 +0100

    Création des classes Enveloppe et Mode

---
+++ gpws.py
@@ -0,0 +1,24 @@
+UP = "0"
+DOWN = "1"
+
+class Enveloppe():
+    def __init__(self, vertexes, alertlevel, priority, flaps, gears, phase):
+        self.vertexes = vertexes
+        self.alertlevel = alertlevel
+        self.priority =priority
+        self.flaps = flaps
+        self.gears = gears
+        self.phase = phase
+
+class Mode():
+    def __init__(self, list_enveloppes):
+        self.list_enveloppes = list_enveloppes
+
+
+
+
+
+
+
+
+
