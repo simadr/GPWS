cff7c61b858eae55649971562453e792a0cdd9fc
Darian <darian.motamed@gmail.com>
2017-01-06 18:20:32 +0100

    [test] Ajout d'une fonction qui plot les modes

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
-import math
+import math, matplotlib.pyplot as plt, matplotlib, numpy as np
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import gpws
 
 def ftmin_to_ms(vz):
     return 0.00508*vz
 
 def ft_to_m(z):
     return 0.3048*z
+
+def plot_mode(mode):
+
+    fig, ax = plt.subplots()
+    patches = []
+    for env in mode.list_enveloppes:
+        polygon = Polygon(env.vertexes, True)
+        patches.append(polygon)
+    p = PatchCollection(patches,  alpha=0.4)
+
+    colors = [i for i in range(len(mode.list_enveloppes))]
+    p.set_array(np.array(colors))
+
+    ax.add_collection(p)
+    plt.autoscale()
 
 def create_testMode1(vzi, vzf, ralti, raltf, nb_points, filename):
     x=0
@@ -30,5 +48,6 @@ def create_testMode1(vzi, vzf, ralti, raltf, nb_points, filename):
         ralt += pas_ralt
     fic.close()
 
-
-create_testMode1(ftmin_to_ms(-1500), ftmin_to_ms(-6225), ft_to_m(2500), ft_to_m(370), 10, "test_mode1.txt")
+plot_mode(gpws.Mode6)
+plt.show()
+#create_testMode1(ftmin_to_ms(-1500), ftmin_to_ms(-6225), ft_to_m(2500), ft_to_m(370), 10, "test_mode1.txt")
